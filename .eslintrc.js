43f31905d57e8cc59ebfae593f10b98ba20b201f
AdamsFontil <adamsfuntil@gmail.com>
2023-08-01 09:02:16 -0400

    test lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,50 @@
-/* eslint-disable no-mixed-spaces-and-tabs */
-
+/* eslint-disable no-undef */
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true,
-    node: true,
-    jest: true,
+  'env': {
+    'browser': true,
+    'es6': true,
+    'jest/globals': true
   },
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: 'latest',
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'ecmaVersion': 2018,
+    'sourceType': 'module'
   },
-  rules: {
-    indent: ['error', 2],
-    eqeqeq: 'error',
+  'plugins': [
+    'react', 'jest'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'eqeqeq': 'error',
     'no-trailing-spaces': 'error',
-    'object-curly-spacing': ['error', 'always'],
-    'arrow-spacing': ['error', { before: true, after: true }],
-    'no-console': 0,
-    'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'never'],
-  },
-};
+    'object-curly-spacing': [
+      'error', 'always'
+    ],
+    'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
+    ],
+    'no-console': 'error',
+    'react/prop-types': 0
+  }
+}
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const app = require('./app') // the actual Express application
 const config = require('./utils/config')
 const logger = require('./utils/logger')
