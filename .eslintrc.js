68c6fc65edb1cec430ebda56cb0df1e955303688
AdamsFontil <adamsfuntil@gmail.com>
2023-08-01 10:52:28 -0400

    make lint work

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,50 +1,44 @@
-/* eslint-disable no-undef */
+/* eslint-disable no-mixed-spaces-and-tabs */
+
 module.exports = {
   'env': {
     'browser': true,
-    'es6': true,
-    'jest/globals': true
+    'commonjs': true,
+    'es2021': true,
+	  'node': true,
+	  'jest': true,
   },
-  'extends': [
-    'eslint:recommended',
-    'plugin:react/recommended'
+  'extends': 'eslint:recommended',
+  'overrides': [
   ],
   'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true
-    },
-    'ecmaVersion': 2018,
-    'sourceType': 'module'
+	  'ecmaVersion': 'latest'
   },
-  'plugins': [
-    'react', 'jest'
-  ],
   'rules': {
-    'indent': [
+	  'indent': [
       'error',
       2
-    ],
-    'linebreak-style': [
+	  ],
+	  'eqeqeq': 'error',
+	  'no-trailing-spaces': 'error',
+	  'object-curly-spacing': [
+      'error', 'always'
+	  ],
+	  'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
+	  ],
+	  'no-console': 0,
+	  'linebreak-style': [
       'error',
       'unix'
-    ],
-    'quotes': [
+	  ],
+	  'quotes': [
       'error',
       'single'
-    ],
-    'semi': [
+	  ],
+	  'semi': [
       'error',
       'never'
-    ],
-    'eqeqeq': 'error',
-    'no-trailing-spaces': 'error',
-    'object-curly-spacing': [
-      'error', 'always'
-    ],
-    'arrow-spacing': [
-      'error', { 'before': true, 'after': true }
-    ],
-    'no-console': 'error',
-    'react/prop-types': 0
+	  ]
   }
 }
