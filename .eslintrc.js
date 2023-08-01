5f1a98083f4269af5e449abe1361f49841aa9526
AdamsFontil <adamsfuntil@gmail.com>
2023-08-01 08:53:25 -0400

    make prettier plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,28 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
 
 module.exports = {
-  'env': {
-    'browser': true,
-    'commonjs': true,
-    'es2021': true,
-	  'node': true,
-	  'jest': true,
+  root: true,
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
   },
-  'extends': 'eslint:recommended',
-  'overrides': [
-  ],
-  'parserOptions': {
-	  'ecmaVersion': 'latest'
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
   },
-  'rules': {
-	  'indent': [
-      'error',
-      2
-	  ],
-	  'eqeqeq': 'error',
-	  'no-trailing-spaces': 'error',
-	  'object-curly-spacing': [
-      'error', 'always'
-	  ],
-	  'arrow-spacing': [
-      'error', { 'before': true, 'after': true }
-	  ],
-	  'no-console': 0,
-	  'linebreak-style': [
-      'error',
-      'unix'
-	  ],
-	  'quotes': [
-      'error',
-      'single'
-	  ],
-	  'semi': [
-      'error',
-      'never'
-	  ]
-  }
-}
+  rules: {
+    indent: ['error', 2],
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'no-console': 0,
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+  },
+};
